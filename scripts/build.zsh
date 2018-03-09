4517b57f3b065fc3756d713d47eb6bdecee5d2cb
Virtual-Machine <darrelm.mailloux@gmail.com>
2018-03-09 18:08:27 -0500

    Rough out build script

--- scripts/build.zsh
+++ scripts/build.zsh
@@ -1,7 +1,11 @@
-# Build C code
+# echo "Building C binary"
+# clang -xc O3 -o ../bin/c-pg ../languages/c/main.c 
 
-# Build Crystak code
+# echo "Building Crystal binary"
+# crystal build --release --no-debug -o ../bin/crystal-pg ../languages/crystal/pg.cr
 
-# Build Go code
+# echo "Building Go binary"
+# go build -ldflags "-s -w" -o ../bin/golang-pg ../languages/go/pg.go
 
-# Move Ruby implementation to ./bin directory
+# echo "Moving Ruby script"
+# cp ../languages/ruby/pg.rb ../bin/ruby-pg.rb
