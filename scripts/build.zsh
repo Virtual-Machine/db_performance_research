752fbc4a81db7e8b0abd8af60309a549c93b04cd
Virtual-Machine <darrelm.mailloux@gmail.com>
2018-03-09 20:08:38 -0500

    Setup to build Crystal using both pg drivers

--- scripts/build.zsh
+++ scripts/build.zsh
@@ -1,8 +1,11 @@
 # echo "Building C binary"
 # clang -xc O3 -o ../bin/c-pg ../languages/c/main.c 
 
-# echo "Building Crystal binary"
-# crystal build --release --no-debug -o ../bin/crystal-pg ../languages/crystal/pg.cr
+echo "Building Crystal-PG"
+crystal build --release --no-debug -Ddb_pg -o bin/crystal-pg languages/crystal/main.cr
+
+echo "Building Crystal-PQ"
+crystal build --release --no-debug -o bin/crystal-pq languages/crystal/main.cr
 
 # echo "Building Go binary"
 # go build -ldflags "-s -w" -o ../bin/golang-pg ../languages/go/pg.go
