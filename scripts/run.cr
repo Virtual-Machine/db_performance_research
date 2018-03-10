977f60b06cecb9dc76bee570a4042cd4f257616a
Virtual-Machine <darrelm.mailloux@gmail.com>
2018-03-10 14:10:07 -0500

    Iterate run script 10 times

--- scripts/run.cr
+++ scripts/run.cr
@@ -5,10 +5,12 @@ connection_string += "&initial_pool_size=1&max_pool_size=1&max_idle_pool_size=1"
 log = "log_file"
 ruby_bin = "/usr/bin/ruby"
 
-get_files "bin/" do |file|
-  if (File.extname file) != ".rb"
-    run_binary file, connection_string, log
-  else
-    run_script ruby_bin, file, log
+10.times do
+  get_files "bin/" do |file|
+    if (File.extname file) != ".rb"
+      run_binary file, connection_string, log
+    else
+      run_script ruby_bin, file, log
+    end
   end
 end
