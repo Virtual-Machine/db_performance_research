9e488247ef8e9678452bc9d54b2b08a3ed9b3f97
Virtual-Machine <darrelm.mailloux@gmail.com>
2018-03-09 22:30:41 -0500

    Rewrite build.zsh as build.cr

---
+++ scripts/build.cr
@@ -0,0 +1,16 @@
+require "crust"
+
+# puts "Building C binary"
+# clang -xc O3 -o ../bin/c-pg ../languages/c/main.c
+
+puts "Building Crystal-PG"
+release_build("-Ddb_pg languages/crystal/main.cr", "bin/crystal-pg")
+
+puts "Building Crystal-PQ"
+release_build("languages/crystal/main.cr", "bin/crystal-pq")
+
+# puts "Building Go binary"
+# go build -ldflags "-s -w" -o ../bin/golang-pg ../languages/go/pg.go
+
+puts "Moving Ruby script"
+`cp languages/ruby/main.rb bin/ruby-pg.rb`
