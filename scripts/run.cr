d9fcc63f3d32f04615263f62c32dcdad5b3e9d21
Virtual-Machine <darrelm.mailloux@gmail.com>
2018-03-10 11:14:46 -0500

    Rewrite run.zsh as run.cr

---
+++ scripts/run.cr
@@ -0,0 +1,14 @@
+require "crust"
+
+connection_string = "postgres://localhost:5432/test?prepared_statements=false"
+connection_string += "&initial_pool_size=1&max_pool_size=1&max_idle_pool_size=1"
+log = "log_file"
+ruby_bin = "/usr/bin/ruby"
+
+get_files "bin/" do |file|
+  if (File.extname file) != ".rb"
+    run_binary file, connection_string, log
+  else
+    run_script ruby_bin, file, log
+  end
+end
