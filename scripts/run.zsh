a8a36f54b908f5c5f151f76f296501c4d4224754
Virtual-Machine <darrelm.mailloux@gmail.com>
2018-03-09 23:32:52 -0500

    Send results to log_file

--- scripts/run.zsh
+++ scripts/run.zsh
@@ -4,12 +4,12 @@ for file in ./bin/*; do
 	# Have file name of binary relative to cwd
 	extension="${file##*.}"
 	if [[ $extension == "rb" ]]; then
-		echo $file
-		ruby $file
+		echo $file >> results/log_file
+		ruby $file >> results/log_file
 	fi
 	if [[ $extension != "rb" ]]; then
-		echo $file
-		$file "postgres://localhost:5432/test?prepared_statements=false&initial_pool_size=1&max_pool_size=1&max_idle_pool_size=1"
+		echo $file >> results/log_file
+		$file "postgres://localhost:5432/test?prepared_statements=false&initial_pool_size=1&max_pool_size=1&max_idle_pool_size=1" >> results/log_file
 	fi
 done
 
