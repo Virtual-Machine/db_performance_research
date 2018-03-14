054600a0bf4fd07b9bed81a6c3ec3b91d0013dd8
Virtual-Machine <darrelm.mailloux@gmail.com>
2018-03-14 02:51:20 -0400

    Refactor summary script

--- scripts/summary.cr
+++ scripts/summary.cr
@@ -1,6 +1,10 @@
 require "crust"
 require "csv"
 
+# ========
+# Helpers
+# ========
+
 def colourize_name(name : String) : String
   case name
   when "crystal-pg", "crystal-pq"
@@ -22,13 +26,32 @@ def clean_float(res : Float64) : String
   return sprintf("%f", res)
 end
 
+# ========
+# ResultSet  =   Container of aggregate stats for one test run
+# ========
+
 struct ResultSet
   property min_name, max_name, min_value, max_value, overall
 
-  def initialize(@min_name : String, @max_name : String, @min_value : Float64, @max_value : Float64, @overall : Hash(String, Float64))
+  def initialize
+    @min_name = ""
+    @max_name = ""
+    @min_value = 9999.0
+    @max_value = 0.0
+    @overall = {"crystal-pg" => 0.0, "crystal-pq" => 0.0, "crystal-libpq" => 0.0, "go" => 0.0, "ruby" => 0.0, "c" => 0.0}
   end
 
-  def print_results(name : Symbol)
+  def set_min_max(subject : String, q_val : Float64)
+    if @min_value > q_val
+      @min_name = subject
+      @min_value = q_val
+    elsif @max_value < q_val
+      @max_name = subject
+      @max_value = q_val
+    end
+  end
+
+  def print_results(name : String)
     puts
     puts name
     puts "==================="
@@ -43,57 +66,55 @@ struct ResultSet
       puts "#{val}  --  #{name}"
     end
 
-    puts "DELTA Fastest Vs Slowest: #{res[-1][1] - res[0][1]}"
+    puts "DELTA Fastest Vs Slowest: #{clean_float(res[-1][1] - res[0][1])}"
   end
 end
 
-def starting_set : ResultSet
-  return ResultSet.new "", "", 0.0, 0.0, {"crystal-pg" => 0.0, "crystal-pq" => 0.0, "crystal-libpq" => 0.0, "go" => 0.0, "ruby" => 0.0, "c" => 0.0}
-end
+# ========
+# generate a hash of String => ResultSet
+# This hash contains a ResultSet per value column in the supplied CSV data
+# ========
 
-macro update_results(*index)
-  {% for i in index %}
-    results["q{{i}}"].overall[subject] += q{{i}}
+def generate_results(data : Array(Array(String))) : Hash(String, ResultSet)
+  results = {} of String => ResultSet
+  data.each do |row|
+    subject = row[0]
+    values = row[1..-1]
 
-    if results["q{{i}}"].min_name == ""
-      results["q{{i}}"].min_name = subject
-      results["q{{i}}"].max_name = subject
-      results["q{{i}}"].min_value = q{{i}}
-      results["q{{i}}"].max_value = q{{i}}
-    else
-      if results["q{{i}}"].min_value > q{{i}}
-        results["q{{i}}"].min_name = subject
-        results["q{{i}}"].min_value = q{{i}}
-      elsif results["q{{i}}"].max_value < q{{i}}
-        results["q{{i}}"].max_name = subject
-        results["q{{i}}"].max_value = q{{i}}
+    values.each_with_index do |val, i|
+      q_index = "q#{i + 1}"
+      q_val = val.to_f
+
+      if !results.has_key?(q_index)
+        results[q_index] = ResultSet.new
       end
+
+      cur_set = results[q_index]
+      cur_set.overall[subject] += q_val
+      cur_set.set_min_max subject, q_val
     end
-  {% end %}
+  end
+  results
 end
 
-results = {
-  "q1": starting_set,
-  "q2": starting_set,
-  "q3": starting_set,
-  "q4": starting_set,
-  "q5": starting_set,
-}
+# ========
+# prints results of each resultset in result collection
+# ========
+
+def print_results(result_collection : Hash(String, ResultSet)) : Nil
+  result_collection.each do |key, rs|
+    rs.print_results key
+  end
+end
+
+# ========
+# for each file in results, generate results and report
+# ========
 
 get_files "results/" do |file|
+  puts file
   csv_data = CSV.parse(File.read(file))
-  csv_data.each do |row|
-    subject = row[0]
-    q1 = row[1].to_f
-    q2 = row[2].to_f
-    q3 = row[3].to_f
-    q4 = row[4].to_f
-    q5 = row[5].to_f
 
-    update_results 1, 2, 3, 4, 5
-  end
-end
-
-results.each do |key, value|
-  value.print_results key
+  results = generate_results csv_data
+  print_results results
 end
