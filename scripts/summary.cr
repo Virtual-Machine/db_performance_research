7c843b9b4f4938539b678b5df66487ef836adf3c
Virtual-Machine <darrelm.mailloux@gmail.com>
2018-03-19 19:07:39 -0400

    Rewrite summary script to use results table

--- scripts/summary.cr
+++ scripts/summary.cr
@@ -1,5 +1,5 @@
 require "crust"
-require "csv"
+require "pg"
 require "yaml"
 
 # ========
@@ -8,7 +8,7 @@ require "yaml"
 
 def colourize_name(name : String) : String
   case name
-  when "crystal-pg", "crystal-pq"
+  when "crystal-pg-1", "crystal-pq-1", "crystal-pg-10", "crystal-pq-10"
     return "\033[0;33m#{name}\033[0m"
   when "crystal-libpq"
     return "\033[0;35m#{name}\033[0m"
@@ -23,98 +23,80 @@ def colourize_name(name : String) : String
   end
 end
 
-def clean_float(res : Float64) : String
-  return sprintf("%f", res)
-end
-
-# ========
-# ResultSet  =   Container of aggregate stats for one test run
-# ========
-
-struct ResultSet
-  property overall, counts
-
-  def initialize(keys : Array(String))
-    @overall = {} of String => Float64
-    keys.each do |key|
-      @overall[key] = 0.0
-    end
-  end
-
-  def print_results(name : String, descriptions : YAML::Any)
-    puts
-    puts descriptions[name]
-    puts "==================="
-    res = [] of Tuple(String, Float64)
-    @overall.each do |rec|
-      res << rec
-    end
-    res.sort! { |x, y| x[1] <=> y[1] }
-
-    res = res.reject { |x| x[1] == 0.0 }
-
-    res.each do |rec|
-      name = colourize_name(rec[0])
-      val = clean_float(rec[1])
-      puts "#{val}  --  #{name}"
-    end
-
-    puts "DELTA Fastest Vs Slowest: #{clean_float(res[-1][1] - res[0][1])}"
-    puts "Overhead: #{clean_float((res[-1][1] / res[0][1]) * 100)}%"
-  end
-end
-
-# ========
-# generate a hash of String => ResultSet
-# This hash contains a ResultSet per value column in the supplied CSV data
-# ========
-
-def generate_results(data : Array(Array(String))) : Tuple(Hash(String, ResultSet), Int32)
-  results = {} of String => ResultSet
-  subjects = data.transpose[0].uniq
-  iterations = data.size / subjects.size
-  data.each do |row|
-    subject = row[0]
-    values = row[1..-1]
-
-    values.each_with_index do |val, i|
-      q_index = "q#{i + 1}"
-      q_val = val.to_f
-
-      if !results.has_key?(q_index)
-        results[q_index] = ResultSet.new subjects
-      end
-
-      cur_set = results[q_index]
-      cur_set.overall[subject] += q_val / iterations
-    end
-  end
-  {results, iterations}
-end
-
-# ========
-# prints results of each resultset in result collection
-# ========
-
-def print_results(result_collection : Hash(String, ResultSet), descriptions : YAML::Any) : Nil
-  result_collection.each do |key, rs|
-    rs.print_results key, descriptions
-  end
-end
-
 query_descriptions = YAML.parse File.read "sql/descriptions.yml"
 
-# ========
-# for each file in results, generate results and report
-# ========
+connection_string = "postgres://localhost:5432/test?prepared_statements=false&initial_pool_size=1&max_pool_size=1&max_idle_pool_size=1"
+pool_size = "1"
 
-get_files "results/" do |file|
-  puts
-  puts file
-  csv_data = CSV.parse(File.read(file))
+DB.open connection_string do |db|
+  puts "Total in Seconds"
+  db.query "select imp_name, sum(seconds) As TotalSeconds from results group by imp_name order by sum(seconds);" do |rs|
+    rs.each do
+      name = rs.read(String)
+      total = rs.read(PG::Numeric)
 
-  results, iterations = generate_results csv_data
-  print_results results, query_descriptions
+      puts "  #{total.to_f}\t#{colourize_name(name)}"
+    end
+  end
+
+  puts "\n#{query_descriptions["q1"]} in Seconds Averaged"
+  db.query "select imp_name, avg(seconds) As AvgSeconds from results where test = 't1' group by imp_name order by avg(seconds);" do |rs|
+    rs.each do
+      name = rs.read(String)
+      total = rs.read(PG::Numeric)
+
+      puts "  #{total.to_f}\t#{colourize_name(name)}"
+    end
+  end
   puts
-  puts "Based on average of #{iterations} runs"
+
+  puts "\n#{query_descriptions["q2"]} in Seconds Averaged"
+  db.query "select imp_name, avg(seconds) As AvgSeconds from results where test = 't2' group by imp_name order by avg(seconds);" do |rs|
+    rs.each do
+      name = rs.read(String)
+      total = rs.read(PG::Numeric)
+
+      puts "  #{total.to_f}\t#{colourize_name(name)}"
+    end
+  end
+
+  puts "\n#{query_descriptions["q3"]} in Seconds Averaged"
+  db.query "select imp_name, avg(seconds) As AvgSeconds from results where test = 't3' group by imp_name order by avg(seconds);" do |rs|
+    rs.each do
+      name = rs.read(String)
+      total = rs.read(PG::Numeric)
+
+      puts "  #{total.to_f}\t#{colourize_name(name)}"
+    end
+  end
+
+  puts "\n#{query_descriptions["q4"]} in Seconds Averaged"
+  db.query "select imp_name, avg(seconds) As AvgSeconds from results where test = 't4' group by imp_name order by avg(seconds);" do |rs|
+    rs.each do
+      name = rs.read(String)
+      total = rs.read(PG::Numeric)
+
+      puts "  #{total.to_f}\t#{colourize_name(name)}"
+    end
+  end
+
+  puts "\n#{query_descriptions["q5"]} in Seconds Averaged"
+  db.query "select imp_name, avg(seconds) As AvgSeconds from results where test = 't5' group by imp_name order by avg(seconds);" do |rs|
+    rs.each do
+      name = rs.read(String)
+      total = rs.read(PG::Numeric)
+
+      puts "  #{total.to_f}\t#{colourize_name(name)}"
+    end
+  end
+
+  puts "\n#{query_descriptions["q6"]} in Seconds Averaged"
+  db.query "select imp_name, avg(seconds) As AvgSeconds from results where test = 't6' group by imp_name order by avg(seconds);" do |rs|
+    rs.each do
+      name = rs.read(String)
+      total = rs.read(PG::Numeric)
+
+      puts "  #{total.to_f}\t#{colourize_name(name)}"
+    end
+  end
 end
